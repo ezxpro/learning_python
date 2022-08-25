bd2a9635e3904993899dec165951398af5662308
ezxpro <ezequiel.ezb@gmail.com>
2022-08-25 20:26:36 -0300

    update lists practice

--- my_files/lists/list-slicing.py
+++ my_files/lists/list-slicing.py
@@ -1,19 +1,25 @@
-list = []
+my_list = []
 
 for i in range(10):
-    list.append("bla bla bla " + str(i))
+    my_list.append("bla bla bla " + str(i))
 
 print("Simple slicing [0:5]")
-print(list[0:5]) #prints index 0 until 4 (gets elements < than final step)
+print(my_list[0:5]) #prints index 0 until 4 (gets elements < than final step)
 
 print("Omitting Initial part picks index 0 as start [:5]")
-print(list[:5]) # omitting initial gets index 0 as start
+print(my_list[:5]) # omitting initial gets index 0 as start
 
 print("Omitting end picks all elements remaining [0:]")
-print(list[0:]) # end picks all items till end of the list
+print(my_list[0:]) # end picks all items till end of the my_list
 
 print("Changing step size to pick only pair indexes [::2)")
-print(list[::2])
+print(my_list[::2])
 
 print("Same but now pick only odd indexes [::1)")
-print(list[::1])
+print(my_list[::1])
+
+print("negative step goes backwards through the my_list [::-1)")
+print(my_list[::-1])
+
+# populate my_list with the output of range()
+my_list = list(range(100))
