9d14b2b78d6466bef9ef2f6f3253eec973633ac4
ezxpro <ezequiel.ezb@gmail.com>
2022-09-01 21:29:54 -0300

    changes to while loop, commented on infinite loop

--- my_files/control_flow/while_loops.py
+++ my_files/control_flow/while_loops.py
@@ -6,6 +6,9 @@ while True:
     if datetime.now().second >=50:
         continue # put this continue here just to show that
                  # it skips one iteration
+                 # This also causes a bug if wait_until > 50
+                 # because the break is skipped, and since the
+                 # variable is never reset it'll loop forever
     if datetime.now().second == wait_until:
         print(f'We are at {wait_until} seconds!')
         break        
