f8f44a271da3c3cfb226975ec63f132fe1a1b8eb
ezxpro <ezequiel.ezb@gmail.com>
2022-10-20 10:35:48 -0300

    got the quickstart script up and running, retrieves a cell from a private spreadsheet in my account

--- labri/apis/google-sheets/getting_started.py
+++ labri/apis/google-sheets/getting_started.py
@@ -1,18 +1,21 @@
 from __future__ import print_function
 
 import os.path
+
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
+os.chdir(os.path.dirname(__file__))
+
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
 # The ID and range of a sample spreadsheet
-SAMPLE_SPREADSHEET_ID = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'
-SAMPLE_RANGE_NAME = 'Class Data!A2:E'
+SAMPLE_SPREADSHEET_ID = '1CAdVx2WD6K_SsjPNqkUKxvuJk2O1An3L4Npe23XoFRk'
+SAMPLE_RANGE_NAME = '!B28:C28'
 
 
 def main():
@@ -30,7 +33,7 @@ def main():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFLow.from_client_secrets_file(
+            flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # save the credentials for the next run
@@ -40,4 +43,23 @@ def main():
         service = build('sheets', 'v4', credentials=creds)
 
         # Call the Sheets API
+        sheet = service.spreadsheets()
+        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID, 
+                                    range=SAMPLE_RANGE_NAME).execute()
+        values = result.get('values', [])
+
+        if not values:
+            print('No data found')
+            return
         
+        print('Name, Major:')
+        for row in values:
+            # Print columns A and E, which correspond to indices 0 and 4
+           # print('%s', '%s' % (row[0], row[4]))
+           #print(row[0], row[4])
+           print(row)
+    except HttpError as err:
+        print(err)
+
+if __name__ == '__main__':
+    main()
