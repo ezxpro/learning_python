b1fe04944b46fdb73888484b578cd96972c7b072
ezxpro <ezequiel.ezb@gmail.com>
2022-10-20 10:47:11 -0300

    added gspread oauth authentication test script and a corresponding readme

---
+++ labri/libraries/gspread/auth/auth_oauth.py
@@ -0,0 +1,10 @@
+import gspread
+import os
+
+os.chdir(os.path.dirname(__file__))
+gc = gspread.oauth(credentials_filename='credentials_oauth.json'
+)
+
+sh = gc.open('Orçamento mensal - Agosto/2022')
+
+print(sh.sheet1.get('B21'))
