edd29e48b61b166c3017afc406cda31773781064
ezxpro <ezequiel.ezb@gmail.com>
2022-10-20 10:13:23 -0300

    added gspread authentication test using google sheets api service account

---
+++ labri/libraries/gspread/auth/gs_svc_auth.py
@@ -0,0 +1,9 @@
+import gspread
+import os
+
+os.chdir(os.path.dirname(__file__))
+gc = gspread.service_account('credentials_svc.json')
+
+sh = gc.open('Orçamento mensal - Agosto/2022')
+
+print(sh.sheet1.get('B21'))
